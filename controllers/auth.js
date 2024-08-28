9d59864e8a13bf187d81fdcc0e47e9766c60e605
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-28 12:05:02 +0300

    Add pre-save middleware to hash user passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,8 @@
 const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
-const bcrypt = require("bcryptjs")
 
 const register = async (req, res) => {
-  const { username, email, password } = req.body
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  const tempUser = { username, email, password: hashedPassword }
-
-  const user = await User.create({ ...tempUser })
+  const user = await User.create({ ...req.body })
   res.status(StatusCodes.CREATED).json({ user })
 }
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema(
   {
@@ -26,5 +27,10 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save", async function (){
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 module.exports = mongoose.model('User', UserSchema);
